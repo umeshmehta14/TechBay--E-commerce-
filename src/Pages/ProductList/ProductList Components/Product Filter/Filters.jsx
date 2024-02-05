ddcdceb51223bc3ba76760670a19a75c107fb8c5
umeshmehta14 <ishaanmehta782@gmail.com>
2024-02-06 00:21:33 +0530

    add: key in map

--- src/Pages/ProductList/ProductList Components/Product Filter/Filters.jsx
+++ src/Pages/ProductList/ProductList Components/Product Filter/Filters.jsx
@@ -22,127 +22,125 @@ const Filters = () => {
   const prices = [500, 1000, 2000, 3000, 4000];
 
   return (
-    <>
-      <div className={`filter-container ${showFilter ? "w-90" : ""}`}>
-        <div className="filter-clear-section">
-          <h2 className="filter-heading">Filters</h2>
-          <div
-            className="cross-filter d-none"
-            onClick={() => dispatch({ type: setShowFilter })}
-          >
-            <RxCross1 />
-          </div>
-          <button
-            className="filter-clr-btn"
-            onClick={() => dispatch({ type: CLEAR_FILTER })}
-          >
-            Clear <MdOutlineFilterAltOff />
-          </button>
+    <div className={`filter-container ${showFilter ? "w-90" : ""}`}>
+      <div className="filter-clear-section">
+        <h2 className="filter-heading">Filters</h2>
+        <div
+          className="cross-filter d-none"
+          onClick={() => dispatch({ type: setShowFilter })}
+        >
+          <RxCross1 />
         </div>
-        <div className="ratings-sort">
-          <label htmlFor="range">
-            <h3>Ratings</h3>
-            <small>{"Less Than " + filters.rating} Star</small>
-          </label>
-          <input
-            type="range"
-            list="rating-inp"
-            min="1"
-            max="5"
-            id="range"
-            value={filters.rating}
-            onChange={(e) =>
-              dispatch({ type: SORT_BY_RATING, payload: e.target.value })
-            }
-          />
-          <datalist id="rating-inp">
-            {ratings?.map((item) => (
-              <option key={item} value={item} label={`<${item}`}></option>
-            ))}
-          </datalist>
-        </div>
-        <div className="category-filter">
-          <h3>Category</h3>
-          {category?.map(({ id, categoryName }) => {
-            return (
-              <label key={id} htmlFor={id}>
-                <input
-                  type="checkbox"
-                  name={categoryName}
-                  checked={filters.category.includes(categoryName)}
-                  id={id}
-                  onChange={() =>
-                    dispatch({
-                      type: SET_CATEGORY_FILTER,
-                      payload: categoryName,
-                    })
-                  }
-                />
-                {categoryName}
-              </label>
-            );
-          })}
-        </div>
-        <div className="price-filter">
-          <h3>Price</h3>
-          {prices?.map((item) => (
-            <label key={item} htmlFor={item + "rs"}>
-              <input
-                type="radio"
-                checked={filters.price === item}
-                name={item}
-                id={item + "rs"}
-                onChange={() => dispatch({ type: SET_PRICE, payload: item })}
-              />
-              Less Than &#8377;{item}
-            </label>
+        <button
+          className="filter-clr-btn"
+          onClick={() => dispatch({ type: CLEAR_FILTER })}
+        >
+          Clear <MdOutlineFilterAltOff />
+        </button>
+      </div>
+      <div className="ratings-sort">
+        <label htmlFor="range">
+          <h3>Ratings</h3>
+          <small>{"Less Than " + filters?.rating} Star</small>
+        </label>
+        <input
+          type="range"
+          list="rating-inp"
+          min="1"
+          max="5"
+          id="range"
+          value={filters.rating}
+          onChange={(e) =>
+            dispatch({ type: SORT_BY_RATING, payload: e.target.value })
+          }
+        />
+        <datalist id="rating-inp">
+          {ratings?.map((item) => (
+            <option key={item} value={item} label={`<${item}`}></option>
           ))}
-        </div>
-        <div className="trending-filter">
-          <h3>Trending</h3>
-          <label htmlFor="trending-sort">
-            <input
-              type="checkbox"
-              checked={filters.trending}
-              name="trending-filter"
-              id="trending-sort"
-              onChange={() => dispatch({ type: SET_TRENDING })}
-            />
-            Only Trending
-          </label>
-        </div>
-        <div className="stock-filter">
-          <h3>In Stock</h3>
-          <label htmlFor="stock-sort">
-            <input
-              type="checkbox"
-              checked={filters.includeOutStock}
-              name="stock-filter"
-              id="stock-sort"
-              onChange={() => dispatch({ type: SET_OUT_OF_STOCK })}
-            />
-            Include Out of Stock
-          </label>
-        </div>
-        <div className="brand-filter">
-          <h3>Brands</h3>
-          {brands?.map((name) => (
-            <label key={name} htmlFor={name}>
+        </datalist>
+      </div>
+      <div className="category-filter">
+        <h3>Category</h3>
+        {category?.map(({ _id, categoryName }) => {
+          return (
+            <label key={_id} htmlFor={_id}>
               <input
                 type="checkbox"
-                name={name}
-                checked={filters.brand.includes(name)}
-                id={name}
+                name={categoryName}
+                checked={filters?.category?.includes(categoryName)}
+                id={_id}
                 onChange={() =>
-                  dispatch({ type: SET_BRAND_FILTER, payload: name })
+                  dispatch({
+                    type: SET_CATEGORY_FILTER,
+                    payload: categoryName,
+                  })
                 }
               />
-              {name}
+              {categoryName}
             </label>
-          ))}
-        </div>
+          );
+        })}
       </div>
-    </>
+      <div className="price-filter">
+        <h3>Price</h3>
+        {prices?.map((item) => (
+          <label key={item} htmlFor={item + "rs"}>
+            <input
+              type="radio"
+              checked={filters.price === item}
+              name={item}
+              id={item + "rs"}
+              onChange={() => dispatch({ type: SET_PRICE, payload: item })}
+            />
+            Less Than &#8377;{item}
+          </label>
+        ))}
+      </div>
+      <div className="trending-filter">
+        <h3>Trending</h3>
+        <label htmlFor="trending-sort">
+          <input
+            type="checkbox"
+            checked={filters.trending}
+            name="trending-filter"
+            id="trending-sort"
+            onChange={() => dispatch({ type: SET_TRENDING })}
+          />
+          Only Trending
+        </label>
+      </div>
+      <div className="stock-filter">
+        <h3>In Stock</h3>
+        <label htmlFor="stock-sort">
+          <input
+            type="checkbox"
+            checked={filters.includeOutStock}
+            name="stock-filter"
+            id="stock-sort"
+            onChange={() => dispatch({ type: SET_OUT_OF_STOCK })}
+          />
+          Include Out of Stock
+        </label>
+      </div>
+      <div className="brand-filter">
+        <h3>Brands</h3>
+        {brands?.map((name) => (
+          <label key={name} htmlFor={name}>
+            <input
+              type="checkbox"
+              name={name}
+              checked={filters.brand.includes(name)}
+              id={name}
+              onChange={() =>
+                dispatch({ type: SET_BRAND_FILTER, payload: name })
+              }
+            />
+            {name}
+          </label>
+        ))}
+      </div>
+    </div>
   );
 };
 
